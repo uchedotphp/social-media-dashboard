6cd31b361f9600e83f5919312a06f61a421cedad
Nwulu Uchechukwu Prince <uchedotphp@gmail.com>
2020-11-10 12:54:17 +0100

    added font-family to app

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,20 @@
 </template>
 
 <script>
-import Dashboard from '@/components/Dashboard';
+import Dashboard from "@/components/Dashboard";
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Dashboard,
   },
-}
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap");
 /* - Font size (Overview Card Headings): 14px  */
 body {
-    background-color: hsl(232, 19%, 15%);
+  font-family: "Inter", sans-serif;
+  background-color: hsl(232, 19%, 15%);
 }
 </style>
